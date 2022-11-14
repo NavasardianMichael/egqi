3d779e8fbe55acfd0c6b4ff6123ba85fc7d76172
NavasardianMichael <navasardianmichael@gmail.com>
2022-11-14 17:39:59 +0400

    excel example added

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,34 +1,38 @@
 import XLSX from "xlsx";
 
 function Input() {
+  const handleChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    const file = e.target.files?.[0] as File;
+    const reader = new FileReader();
 
-    const handleChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
-        const file = e.target.files?.[0] as File
-        const reader = new FileReader()
+    reader.onload = (e) => {
+      const data = e.target?.result;
+      const workbook = XLSX.read(data, {
+        type: "binary",
+      });
+      workbook.SheetNames.forEach((sheet) => {
+        let rowObject = XLSX.utils.sheet_to_json(workbook.Sheets[sheet]);
+        console.log(rowObject);
+      });
+    };
 
-
-        reader.onload = (e) => {
-            const data = e.target?.result
-            const workbook = XLSX.read(data, {
-                type: 'binary'
-            })
-           workbook.SheetNames.forEach(sheet => {
-            let rowObject = XLSX.utils.sheet_to_json(workbook.Sheets[sheet])
-            console.log(rowObject)
-           })
-
-        }
-
-        reader.readAsBinaryString(file)
-    }
+    reader.readAsBinaryString(file);
+  };
 
   return (
-    <div className="my-3 w-75">
-        <label htmlFor="formFile" className="form-label">
-          We generate the index according to imported excel file with specific
-          shape of data
-        </label>
-        <input onChange={handleChange} className="form-control" type="file" id="formFile" accept=".xlsx, .xls, .csv" />
+    <div className="my-3">
+      <label htmlFor="formFile" className="form-label">
+        We generate the index according to imported excel file with specific
+        shape of data
+      </label>
+      <input
+        onChange={handleChange}
+        className="form-control w-100"
+        style={{ maxWidth: "300px" }}
+        type="file"
+        id="formFile"
+        accept=".xlsx, .xls, .csv"
+      />
     </div>
   );
 }
